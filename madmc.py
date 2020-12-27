f009969dd3b93b6fa3641480d50df46bd0cf0b26
xiewf2019 <714253971@qq.com>
2020-12-27 14:51:14 +0100

    Update madmc.py

--- madmc.py
+++ madmc.py
@@ -1,3 +1,20 @@
 import numpy as np
 def alea(n,m):
+	#genere des exemple aleatoirement
 	return m/4*numpy.random.randn(n, 2)+m
+
+def algoNaif(z,n):
+	res=[]
+	for i in range(n):
+		state=False
+		for j in range(n):
+			if compare(z[i],z[j]):
+				state = True
+		if state== False :
+			res.append(z[i])
+
+def compare(a,b):
+	if b[0]<a[0] and b[1]<=a[1] :
+		return True
+	if b[0]<=a[0] and b[1]<a[1] :
+		return True
